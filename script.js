00a98e587231d6c1152ab17fef98ce7e7af4cf8b
Masagus Ahmad Ramadhan <agusyantosugiyanto@gmail.com>
2026-03-13 00:53:18 +0700

    create about section

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ function goToPage(target) {
     console.log(target)
     location.href = "#"+target
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("about-slides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "flex";  
+  slides[slideIndex-1].style.animation = "slide 1s";  
+  console.log(slideIndex)
+}
