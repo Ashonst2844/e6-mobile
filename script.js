a5ff1431b734e1e4d346c31fe67e3af1d8ba5ac4
Masagus Ahmad Ramadhan <agusyantosugiyanto@gmail.com>
2026-03-14 20:50:17 +0700

    add preview feature and maps section

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "flex";  
   slides[slideIndex-1].style.animation = "slide 1s";  
-  console.log(slideIndex)
 }
 
 const category = document.querySelectorAll('input[name="operator"]');
@@ -35,16 +34,25 @@ category.forEach(radio => {
     if (target) {
         target.classList.add('active');
     }
-    console.log(target.id)
-
   });
 });
+
+let charName = ""
 
 fetch('src/operatorList.json')
     .then(response => response.json())
     .then(data => {
         const attacker = document.querySelector('#attackerList');
         const defender = document.querySelector('#defenderList');
+        const preview = document.getElementById('operatorPreview')
+        const namePreview = document.getElementById('namePreview')
+        const imgPreview = document.getElementById('imagePreview')
+        const primary = document.getElementById('primary')
+        const secondary = document.getElementById('secondary')
+        const ability = document.getElementById('ability')
+        const primaryPreview = document.getElementById('primaryWeapon')
+        const secondaryPreview = document.getElementById('secondaryWeapon')
+        const abilityPreview = document.getElementById('abilitySkill')
         data[0].attacker.forEach((item) => {
             const cards = document.createElement("div")
             cards.setAttribute("class", "operator-cards")
@@ -59,6 +67,18 @@ fetch('src/operatorList.json')
             cards.appendChild(title)
             cards.appendChild(shining)
             attacker.appendChild(cards)
+
+            cards.addEventListener('click', () => {
+              imgPreview.setAttribute("src", `src/img/operators/attacker/${item.name}.avif`)
+              namePreview.innerText = item.name
+              primary.style.backgroundImage = `url(src/img/ability/${item.name}/primary.webp)`
+              secondary.style.backgroundImage = `url(src/img/ability/${item.name}/secondary.webp)`
+              ability.setAttribute("src", `src/img/ability/${item.name}/ability.webp`)
+              primaryPreview.innerText = item.primary
+              secondaryPreview.innerText = item.secondary
+              abilityPreview.innerText = item.ability
+              preview.style.display = "flex"
+            });
         });
         data[0].defender.forEach((item) => {
             const cards = document.createElement("div")
@@ -74,7 +94,44 @@ fetch('src/operatorList.json')
             cards.appendChild(title)
             cards.appendChild(shining)
             defender.appendChild(cards)
+
+            cards.addEventListener('click', () => {
+              imgPreview.setAttribute("src", `src/img/operators/defender/${item.name}.avif`)
+              namePreview.innerText = item.name
+              primary.style.backgroundImage = `url(src/img/ability/${item.name}/primary.webp)`
+              secondary.style.backgroundImage = `url(src/img/ability/${item.name}/secondary.webp)`
+              ability.setAttribute("src", `src/img/ability/${item.name}/ability.webp`)
+              primaryPreview.innerText = item.primary
+              secondaryPreview.innerText = item.secondary
+              abilityPreview.innerText = item.ability
+              preview.style.display = "flex"
+            });
+        });
+    })
+    .catch(error => console.error('Error loading JSON data:', error));
+
+fetch('src/mapList.json')
+    .then(response => response.json())
+    .then(data => {
+        const container = document.querySelector('#mapsBox');
+        data.forEach((item) => {
+            const cards = document.createElement("div")
+            cards.setAttribute("class", "map-cards")
+            cards.style.backgroundImage = `url(src/img/maps/${item.name}.avif)`
+            
+            const title = document.createElement("span")
+            title.innerText = item.name
+
+            cards.appendChild(title)
+            container.appendChild(cards)
         });
 
     })
     .catch(error => console.error('Error loading JSON data:', error));
+
+document.querySelector("#close").addEventListener('click', ()=> {
+  const modal = document.getElementById('operatorPreview');
+  if (modal) {
+    modal.style.display = 'none';
+  }
+})
